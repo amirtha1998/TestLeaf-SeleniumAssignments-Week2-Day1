627b76dabd718cfd9410d67ab7307ebe0bffb2a9
amirtha1998 <79782011+amirtha1998@users.noreply.github.com>
2022-03-27 13:12:31 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,37 @@
+package week2.day1.assignment;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead {
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Amirthavalli");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("S");
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Amirthavalli");
+		driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("Testing");
+		driver.findElement(By.id("createLeadForm_description")).sendKeys("Selenium Testing March Batch");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("amirtha98@gmail.com");
+		WebElement eleSource = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select dd = new Select(eleSource);
+		dd.selectByVisibleText("New York");
+		driver.findElement(By.name("submitButton")).click();
+		String title = driver.getTitle();
+		System.out.println(title);
+	}
+}
